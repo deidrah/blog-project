57d6fb1ba25dd61d7ebaba0368074d39588b2e46
Deidrah <dominikastasik93@gmail.com>
2021-03-22 15:19:13 +0100

    adding generateTags function

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@ function generateTitleLinks(){
     const articleTitle = article.querySelector(optTitleSelector).innerHTML;
     console.log(articleTitle);
     /* create HTML of the link */
-    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    const linkHTML = `<li><a href="#${articleId}"><span>${articleTitle}</span></a></li>`;
     console.log(linkHTML);
     /* insert link into titleList */
     html = html + linkHTML;
@@ -70,28 +70,35 @@ generateTitleLinks();
 
 function generateTags(){
   /* find all articles */
-
+  const articles = document.querySelectorAll(optArticleSelector);
   /* START LOOP: for every article: */
-
+  for (let article of articles) {
     /* find tags wrapper */
-
+    const tagsWrapper = article.querySelector(optArticleTagsSelector);
+    console.log(tagsWrapper);
     /* make html variable with empty string */
-
+    let html = '';
     /* get tags from data-tags attribute */
-
+    let articleTags = article.getAttribute('data-tags');
+    console.log(articleTags);
     /* split tags into array */
-
+    const articleTagsArray = articleTags.split(' ');
+    console.log(articleTagsArray);
     /* START LOOP: for each tag */
-
+    for (let tag of articleTagsArray) {
+      console.log(tag);
       /* generate HTML of the link */
-
+      const linkHTML = `<li><a href='#tag-${tag}'>${tag}</a></li>`; 
+      console.log(linkHTML);
       /* add generated code to html variable */
-
+      html = html + '  ' + linkHTML;
+      console.log(html);
     /* END LOOP: for each tag */
-
+    }
     /* insert HTML of all the links into the tags wrapper */
-
+    tagsWrapper.innerHTML = html;
   /* END LOOP: for every article: */
+  }
 }
 
 generateTags();
